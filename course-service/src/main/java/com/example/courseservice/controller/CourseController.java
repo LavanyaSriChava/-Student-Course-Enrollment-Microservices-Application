932e6f4b91099f9438966cdda4deaf6ac006e508
LavanyaSriChava <lavanya.chava728@gmail.com>
2026-06-29 00:03:15 +0530

    Docker implemented

---
+++ course-service/src/main/java/com/example/courseservice/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.example.courseservice.controller;
+
+import com.example.courseservice.model.Course;
+import com.example.courseservice.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @PostMapping
+    public Course addCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+}
